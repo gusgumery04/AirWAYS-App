7a3061a5dc8bbb6faca92051b0166765e027d584
emanducranwell <emanducranwell@gmail.com>
2026-03-07 12:49:22 +0000

    Open & Closed set colours
    Grid is now drawn!
    
    Bugs Fixed:
    - Port wasnt shwowing, i & j mix up.
    - wrote closedSet twice instead of openSet
    All fixed

--- airways-algorithm-01/sketch.js
+++ airways-algorithm-01/sketch.js
@@ -11,26 +11,8 @@ var start;
 var end;
 
 var w,h;
+var col; //colour
 
-//adding a constructor function So we change stuff along the way
-function Spot(i,j){
-
-    this.x = i;
-    this.y = j;
-
-    this.f = 0;
-    this.g = 0;
-    this.h = 0;
-
-    this.show = function (){
-
-        fill(255);
-        strokeWeight(1);
-
-        rect((this.x *w), (this.y *h), w,h);
-
-    }
-}
 
 function setup() {
     createCanvas(400, 400);
@@ -47,8 +29,8 @@ function setup() {
     }
 
     for (var i = 0; i < cols; i++){
-        for (var j = 0; i < rows; j++){
-            grid[i][j] = new Spot();
+        for (var j = 0; j < rows; j++){
+            grid[i][j] = new Spot(i,j);
         }   
     }
 
@@ -66,23 +48,53 @@ function setup() {
 
 function draw() {
 
-  if(openSet.length > 0){
+    if(openSet.length > 0){
 
 
-  } else{
+    } else{
 
-  } 
+    } 
   
-  background(220);
+    background(220);
 
-  for (var i = 0; i < cols; i++){
-    for (var j = 0; i < rows; j++){
-        grid[i][j].show();
-    }   
+    for (var i = 0; i < cols; i++){
+        for (var j = 0; j < rows; j++){
+         grid[i][j].show(color(255));
+        }   
+    }
+
+    for (var i = 0; i < closedSet.length; i++){
+        closedSet[i].show(color(255,0,0));
+    }
+
+    for (var i = 0; i < openSet.length; i++){
+        openSet[i].show(color(0,255,0));
+    }
+
+
 }
 
+//adding a constructor function so we change stuff along the way
+function Spot(i,j){
 
+    this.x = i;
+    this.y = j;
 
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+
+    //show function
+
+    this.show = function (col){
+
+        fill(col);
+        noStroke();
+        strokeWeight(1);
+
+        rect((this.x *w), (this.y *h), w,h);
+
+    } //END spot function
 }
 
 
